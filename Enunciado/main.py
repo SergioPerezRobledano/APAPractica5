d7b535455a6f018ff4550aeb8f62d069ce168f56
Mikeey84 <milope21@ucm.es>
2025-12-03 13:09:29 +0100

    comparacion con MLPClassifier va de locos

--- Enunciado/main.py
+++ Enunciado/main.py
@@ -1,6 +1,7 @@
 # run_merge_csv.py
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.neural_network import MLPClassifier 
 
 from Utils import unir_csv_en_carpeta, cargar_y_preprocesar_csv
 from public_test import MLP_test_step_multi
@@ -28,6 +29,14 @@ if __name__ == "__main__":
     y_train_encoded = encoder.fit_transform(y_train.reshape(-1,1))
     y_test_encoded  = encoder.transform(y_test.reshape(-1,1))
 
+
+    # 4.5 Crear parametros 
+    hidden_layers_global = [50, 40, 30]
+    alpha_global = 0.0
+    num_ite_global = 5000
+    lambda_global = 0.92606
+    verbose_global = 200
+
     # 5 Ejecutar test con tu MLP personalizado
     print("Test 1: Calculando para lambda = 0")
     MLP_test_step_multi(
@@ -37,9 +46,29 @@ if __name__ == "__main__":
         y_train_encoded,
         X_test,
         y_test_encoded,
-        0.0,      # alpha
-        5000,   # num_ite
-        0.92606,# lambda_
-        200,    # verbose
-        hidden_layers=[50, 30, 20]
+        alpha_global,        # alpha
+        num_ite_global,    # num_ite
+        lambda_global,     # lambda_
+        verbose_global,    # verbose para mostrar las iteracciones
+        hidden_layers=hidden_layers_global 
     )
+
+    mlp_sk = MLPClassifier(hidden_layer_sizes=hidden_layers_global,
+                           activation="logistic",
+                           solver="sgd",
+                           max_iter = num_ite_global,
+                           learning_rate='constant', 
+                           learning_rate_init = lambda_global,
+                           n_iter_no_change = num_ite_global, 
+                           alpha = alpha_global,
+                           verbose=True,
+                           random_state=0,
+                           )
+
+    mlp_sk.fit(X_train,y_train_encoded) # Para entrenar a la red con los dato s que le pasamos 
+    acc = mlp_sk.score(X_test,y_test_encoded) # Esto saca la precision del modelo con respecto a los datos supuestos del test
+
+    if(acc > 0.80):
+        print("Guay del paraguay: " + str(acc))
+    else: 
+        print("MALMALMAL: " + str(acc))
